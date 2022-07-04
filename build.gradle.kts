97a30dbb2217e20abba01599cee6c7baa9c608d7
Alexander Broadbent <alexbroadbent14@gmail.com>
2022-07-04 09:37:21 +0100

    Stabilise build process

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,13 +119,11 @@ subprojects {
             }
         }
 
-        val NEXUS_USERNAME: String? by properties
-        val NEXUS_PASSWORD: String? by properties
         repositories {
             maven {
                 credentials {
-                    username = NEXUS_USERNAME
-                    password = NEXUS_PASSWORD
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
                 }
                 name = this@subprojects.name
                 setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
